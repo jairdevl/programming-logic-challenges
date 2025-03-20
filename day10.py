68afe123685c74f88913114612f5bae452938b0a
Jaireua <jairenriquez30@gmail.com>
2025-03-20 16:06:15 -0500

    update challenges

---
+++ day10.py
@@ -0,0 +1,14 @@
+# Program that checks if a year is a leap year
+
+year = int(input("Enter a year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+    else:
+        print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
