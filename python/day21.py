d2eb32edc871d8a8459e70c9cc31f05b3b6e4fe0
Jaireua <jairenriquez30@gmail.com>
2025-03-27 17:20:37 -0500

    new challenges

---
+++ python/day21.py
@@ -0,0 +1,13 @@
+# Data structure for creating and operating on an array
+# matrix de 3x3
+array = [
+    [1, 2, 3],
+    [4, 5, 6], 
+    [7, 8, 9]]
+
+element = array[0][1]
+
+addition = sum(array[1])
+
+print(f"The element in the row 1 and column 2 is: {element}")
+print(f"The addition of the row 1 is: {addition}")
