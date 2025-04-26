3b60f3991683a0ad4db7a2716e494ae9a6b12a3e
fazt <fazt@kali.bbrouter>
2025-04-26 08:48:47 -0500

    new challenges

---
+++ movies_management_system.py
@@ -0,0 +1,17 @@
+"""
+Reto del día: Crea un programa que simule un sistema de gestión
+de suscripciones para una plataforma de streaming de películas.
+El usuario debe poder registrar nuevos suscriptores, eliminar 
+suscripciones y consultar el estado de cada cuenta. Cada suscriptor
+debe tener un nombre, correo electrónico, tipo de suscripción 
+(mensual, trimestral, anual) y fecha de inicio. Restricción: 
+Implementa una validación para que no se puedan añadir suscriptores 
+con correos duplicados y que las fechas de inicio sean válidas. Además,
+el programa debe permitir filtrar suscriptores por tipo de suscripción 
+y generar un informe mensual que muestre la cantidad total de suscriptores 
+por tipo y los ingresos generados. También incluye una opción de enviar 
+orreos electrónicos de renovación a los suscriptores cuya suscripción
+esté próxima a vencer, recordándoles la fecha y el costo de renovación
+correspondiente.
+"""
+
