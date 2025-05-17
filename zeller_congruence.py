7b7f5a9a8db423c4d80de18422f15f69c6506811
jaidevl <jairenriquez30@gmail.com>
2025-05-16 22:27:20 -0500

    creating singup

--- zeller_congruence.py
+++ zeller_congruence.py
@@ -4,12 +4,31 @@ def zeller_congruence(day, month, year):
         month += 12
         year -= 1
     
+    # Variable keys
     q = day
     m = month
     k = year % 100
     j = year // 100
-    h = (q +(13*(m+1))// 5 + k + k//4 + j//4-2*j)%7
-    return h
 
-# Example usage 
-print(f"h= {zeller_congruence(6, 9, 2010)}")
+    # Main formula
+    h = (q +(13*(m+1))// 5 + k + k//4 + j//4-2*j)%7
+
+    # Mapping the days of the week
+    days = ["sábado", "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+    return days[h]
+
+# Ask user for data
+try:
+    day = int(input("Enter the day (1-31): "))
+    month = int(input("Enter the month (1-12): "))
+    year = int(input("Enter the year (ej. 2025): "))
+
+    # Get the day of the week
+    day_name = zeller_congruence(day, month, year)
+
+    if day_name:
+        print(f"The day {day}/{month}/{year} is {day_name}")
+    else:
+        print("¡Invalid date. Please enter a valid date")
+except ValueError:
+    print("Error: Please enter valid numbers")
